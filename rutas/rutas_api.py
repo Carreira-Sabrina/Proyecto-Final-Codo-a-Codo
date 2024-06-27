3d853ea574b470d4449c2cb50754e8478bed36dc
Carreira-Sabrina <sabrina-viviana-carreira@gmail.com>
2024-06-27 07:23:18 -0300

    La api ya puede devolver un cliente por id

--- rutas/rutas_api.py
+++ rutas/rutas_api.py
@@ -13,8 +13,21 @@ def api_obtener_clientes():
     clientes = Cliente.obtener_todos_de_tabla()
     
     #Cuidadito ! clientes recibe una lista de objetos de tipo Cliente, para pasárselo a jsonify() se convierte cada objeto en diccionario en la linea sgte
+    # vars() devuelve un diccionario con los atributos de la instancia
     lista_diccionarios_clientes = [vars(cliente) for cliente in clientes]
     
     #Devuelvo un json que será consumido... en alguna parte
     return jsonify(lista_diccionarios_clientes)
 
+
+@app.route("/api/obtener_cliente_por_id/<int:id>")
+#Nota mental: no olvidar poner en mismo parametro en la url y entre los paréntesis de la funcion ...
+def api_obtener_cliente_por_id(id):
+    #Devuelve un objeto de tipo cliente
+    cliente = Cliente.obtener_objeto_de_tabla(id)
+    #Cuidadito ! clientes recibe una lista de objetos de tipo Cliente, para pasárselo a jsonify() se convierte cada objeto en diccionario en la linea sgte
+    # vars() devuelve un diccionario con los atributos de la instancia
+    diccionario_cliente = vars(cliente)
+    
+    #Devuelvo un json que será consumido... en alguna parte
+    return jsonify(diccionario_cliente)
